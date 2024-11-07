02562c45ea9b15c48fa9e54573895fccd8fda4ca
Ramduth <ramduthappu2001@gmail.com>
2024-11-07 23:44:00 +0530

    Third commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express=require("express")
+const mongoose = require('mongoose')
 const bodyparser=require("body-parser");
 const app=express()
-const port = process.env.PORT || 4000 
+const port = process.env.PORT || 4000
+require('dotenv').config() 
 const cors=require("cors")
 app.use(bodyparser.json())
 const route=require("./routes/auth")
 app.use(cors())
 app.use("/",route)
-const db=require("./config/db")
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=> console.log(`Connection Succesfull`))
+.catch((err)=> console.log(err))
+
 app.listen(port,function() {
-    console.log("port is working on 4000");
+    console.log(`port is working on ${port}`);
     
 })
