1eae636d89d60cac87f18cf72e06be9cc341b500
Ramduth <ramduthappu2001@gmail.com>
2024-11-07 22:53:19 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express=require("express")
-const bodyparser=require("body-parser")
+const bodyparser=require("body-parser");
 const app=express()
+const port = process.env.PORT || 4000 
 const cors=require("cors")
 app.use(bodyparser.json())
 const route=require("./routes/auth")
 app.use(cors())
 app.use("/",route)
 const db=require("./config/db")
-app.listen(4000,function() {
+app.listen(port,function() {
     console.log("port is working on 4000");
     
 })
