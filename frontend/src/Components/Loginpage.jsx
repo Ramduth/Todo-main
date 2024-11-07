8f0cc6fb2546ac32f5faded4f391d9eb8b1ec9dd
Ramduth Rajesh <113904110+Ramduth@users.noreply.github.com>
2024-11-08 00:01:54 +0530

    Update Loginpage.jsx

--- frontend/src/Components/Loginpage.jsx
+++ frontend/src/Components/Loginpage.jsx
@@ -19,7 +19,7 @@ const handleChange = (e) => {
         setErrors(prevErrors => ({ ...prevErrors, [name]: '' }));
     };
 
-   const url = "https://todo-main-backend1.onrender.com"
+   const url = "https://todo-main-backend2.onrender.com"
  const validateForm = () => {
     let formIsValid = true;
         let errorMessages = {};
